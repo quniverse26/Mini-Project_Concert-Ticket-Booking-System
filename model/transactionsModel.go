cf8bb58a157f4d86fade0908e400a7b6784ca457
quniverse26 <quniverse26@gmail.com>
2023-04-27 12:33:51 +0700

    add to Mini-Project_Concert-Ticket-Booking-System

--- model/transactionsModel.go
+++ model/transactionsModel.go
@@ -8,9 +8,9 @@ import (
 type Transactions struct {
 	gorm.Model
 	Status string `json:"status" form:"status"`
-	Users []User `json:"-" form:"-"`
-	Tickets []Ticket `json:"-" form:"-"`
-	Bookings []Booking `json:"-" form:"-"`
+	Users []User `json:"-" form:"-" gorm:"foreignKey:id"`
+	Tickets []Ticket `json:"-" form:"-" gorm:"foreignKey:id"`
+	Bookings []Booking `json:"-" form:"-" gorm:"foreignKey:id"`
 }
 
 var transactions []Transactions
